850c1bf867db9b0ef80dc9cb72ec094eaf00a286
Nikita081 <samsonov0808@rambler.ru>
2015-05-12 15:21:53 +0400

    server

---
+++ sourses/MultyThreadingServer/src/myServer/MultyThreadingServer.java
@@ -0,0 +1,52 @@
+package myServer;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultyThreadingServer {
+	private static ServerSocket serverSocket = null;
+	private static Socket clientSocket = null;
+	private static final int maxClients = 100;
+	private static final ClientThread[] threads = new ClientThread[maxClients];
+
+	public static void main(String[] args) {
+
+		// enter port number
+		int portNumber = 2222;//Integer.valueOf(args[0]).intValue();
+
+		/* open server socket with default port (portNumber) */
+		try {
+			serverSocket = new ServerSocket(portNumber);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		System.out.println("Server started");
+		/* Create a client socket for each connecting, and pass it to new thread */
+		while(true){
+			
+			try {
+				clientSocket = serverSocket.accept();
+				int i = 0;
+				for(i=0;i<maxClients;i++){
+					if(threads[i] == null){
+						(threads[i] = new ClientThread(clientSocket,threads)).start();
+						break;
+					}
+				}
+				if(i==maxClients){
+					PrintStream out = new PrintStream(clientSocket.getOutputStream());
+					out.println("Srever too busy, please, try later");
+					out.close();
+					clientSocket.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			
+			
+		}
+	}
+
+}
