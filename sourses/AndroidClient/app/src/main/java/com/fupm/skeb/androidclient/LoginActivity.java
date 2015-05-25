882150b57c4d99e51f69e4f931a0dff15e0d88aa
Buklujun <Professor240@yandex.ru>
2015-05-25 20:38:45 +0300

    aouth

--- sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/LoginActivity.java
+++ sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/LoginActivity.java
@@ -135,19 +135,19 @@ public class LoginActivity extends FragmentActivity {
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
+//            getView().findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    VKSdk.authorize(sMyScope, true, false);
+//                }
+//            });
+
             getView().findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    VKSdk.authorize(sMyScope, true, false);
+                    VKSdk.authorize(sMyScope, true, true);
                 }
             });
-
-//            getView().findViewById(R.id.force_oauth_button).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    VKSdk.authorize(sMyScope, true, true);
-//                }
-//            });
         }
     }
     public static class LogoutFragment extends android.support.v4.app.Fragment {
