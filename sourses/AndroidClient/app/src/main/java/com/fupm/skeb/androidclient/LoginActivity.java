f9792cae2af9f1a7b6fe8788991e1a0f8f443d97
Buklujun <Professor240@yandex.ru>
2015-05-26 15:14:10 +0300

    check

--- sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/LoginActivity.java
+++ sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/LoginActivity.java
@@ -135,12 +135,7 @@ public class LoginActivity extends FragmentActivity {
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
-//            getView().findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    VKSdk.authorize(sMyScope, true, false);
-//                }
-//            });
+
 
             getView().findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
                 @Override
