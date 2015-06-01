99cf65da495f40b63f53f3ea70e84a279602ce01
BorisovDm <abdima2010@mail.ru>
2015-06-01 20:54:58 +0300

    ChangeBackground

--- sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/ChangeBackground.java
+++ sourses/AndroidClient/app/src/main/java/com/fupm/skeb/androidclient/ChangeBackground.java
@@ -1,32 +1,33 @@
 package com.fupm.skeb.androidclient;
 
-import android.content.SharedPreferences;
-import android.content.Context;
+public class ChangeBackground{
 
-
-public class ChangeBackground {
-
-    public static final String APP_PREFERENCES = "mysettings";
-    public static final String KEY_RADIOBUTTON_INDEX = "SAVED_RADIO_BUTTON_INDEX";
-/*
-    private SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-    int Index = mSettings.getInt(KEY_RADIOBUTTON_INDEX, 0);
-
-    switch(Index) {
-        case 0: return R.drawable.ferma;
-        case 1: return R.drawable.p1;
-        case 2: return R.drawable.p2;
-        case 3: return R.drawable.p3;
-        case 4: return R.drawable.p4;
-        case 5: return R.drawable.p5;
-        case 6: return R.drawable.p6;
-        case 7: return R.drawable.p7;
-        case 8: return R.drawable.p8;
-        case 9: return R.drawable.p9;
-        default: return R.drawable.ferma;
+    public int choose(int key) {
+        switch (key) {
+            case 0:
+                return R.drawable.ferma;
+            case 1:
+                return R.drawable.p1;
+            case 2:
+                return R.drawable.p2;
+            case 3:
+                return R.drawable.p3;
+            case 4:
+                return R.drawable.p4;
+            case 5:
+                return R.drawable.p5;
+            case 6:
+                return R.drawable.p6;
+            case 7:
+                return R.drawable.p7;
+            case 8:
+                return R.drawable.p8;
+            case 9:
+                return R.drawable.p9;
+            default:
+                return R.drawable.ferma;
+        }
     }
-
-*/
 }
 
 
